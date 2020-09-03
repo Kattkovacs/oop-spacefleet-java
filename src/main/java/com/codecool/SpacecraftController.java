a20bd1f788db630a9775ad874872554db85f17a4
Kattkovacs <kattkovacs@gmail.com>
2020-09-03 15:35:34 +0200

    move implementation to App

--- src/main/java/com/codecool/SpacecraftController.java
+++ src/main/java/com/codecool/SpacecraftController.java
@@ -1,22 +1,19 @@
 package com.codecool;
 
-import com.codecool.ships.CargoShip;
-import com.codecool.ships.DiscoveryShip;
 import com.codecool.ships.Spacecraft;
+
+import java.util.HashSet;
 import java.util.Set;
 
-import static com.codecool.ShipType.CARGO_SHIP;
-import static com.codecool.ShipType.DISCOVERY_SHIP;
-
 public class SpacecraftController {
-    private Set<Spacecraft> spacecrafts;
+    private Set<Spacecraft> spacecrafts = new HashSet<>();
 
     public Set<Spacecraft> getSpacecrafts() {
         return this.spacecrafts;
     }
 
-    public void addSpacecrafts(){
-        spacecrafts.add(new CargoShip("cs1", "CargoKing", 2152, CARGO_SHIP, 150));
-        spacecrafts.add(new DiscoveryShip("ds1", "DiscoveryOne", 2201, DISCOVERY_SHIP, 450));
+    public void addSpacecrafts(Spacecraft spacecraft){
+        spacecrafts.add(spacecraft);
+        spacecrafts.add(spacecraft);
     }
 }
--- src/main/java/com/codecool/starfleet/app/App.java
+++ src/main/java/com/codecool/starfleet/app/App.java
@@ -1,6 +1,11 @@
 package com.codecool.starfleet.app;
 
 import com.codecool.SpacecraftController;
+import com.codecool.ships.CargoShip;
+import com.codecool.ships.DiscoveryShip;
+
+import static com.codecool.ShipType.CARGO_SHIP;
+import static com.codecool.ShipType.DISCOVERY_SHIP;
 
 /**
  * Hello world!
@@ -11,5 +16,7 @@ public class App
     public static void main( String[] args )
     {
         SpacecraftController spacecraftController = new SpacecraftController();
+        spacecraftController.addSpacecrafts(new CargoShip("cs1", "CargoKing", 2152, CARGO_SHIP, 150));
+        spacecraftController.addSpacecrafts(new DiscoveryShip("ds1", "DiscoveryOne", 2201, DISCOVERY_SHIP, 450));
     }
 }
